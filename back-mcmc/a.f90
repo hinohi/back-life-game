3e79af266c6ec2e3a03093667940b1c046672e6f
Daiju Nakayama <42.daiju@gmail.com>
2017-11-05 23:52:22 +0900

    バグ修正

--- back-mcmc/a.f90
+++ back-mcmc/a.f90
@@ -1,13 +1,13 @@
 program main
   use random
   implicit none
-  integer, parameter :: L=16, N=L*L
-  double precision, parameter :: XX=0.8d0
+  integer, parameter :: L=8, N=L*L
+  double precision, parameter :: XX=0.5d0
   integer :: target_spin(0:N-1), spin0(0:N-1), spin1(0:N-1), o(0:N)
   double precision :: hist(0:N), f(0:N), k
-  integer :: seed, mc, E, ki
+  integer :: seed, p, mc, E, ki
 
-  read(*, *) seed
+  read(*, *) seed, p
   call warmDRN(seed)
   call init_spin
 
@@ -21,14 +21,14 @@ program main
   do
      hist(:) = 0.d0
      do
-        do mc = 1, N*N
+        do mc = 1, N*N*10
            call one_mc
         end do
         if (is_flat() == 1) then
            exit
         end if
      end do
-     if (k < 1.d-8) then
+     if (k < 1.d-3) then
         exit
      end if
      k = k * 0.5d0
@@ -41,17 +41,19 @@ contains
   subroutine output
     integer :: i
     character(99) :: name
-    write(name, "('data/L', i4.4, '_seed', i4.4, '.spin')") L, seed
+    write(name, "('data/L', i4.4, '_seed', i4.4, '_p', i3.3, '.spin')") L, seed, p
     open(10, file=trim(name), action="write")
     do i = 0, L - 1
        write(10, "(9999i1)") target_spin(i*L:(i+1)*L)
     end do
     close(10)
 
-    write(name, "('data/L', i4.4, '_seed', i4.4, '.hist')") L, seed
+    write(name, "('data/L', i4.4, '_seed', i4.4, '_p', i3.3, '.hist')") L, seed, p
     open(10, file=trim(name), action="write")
     do i = 0, N
-       write(10, *) i, f(i)
+       if (o(i) == 1) then
+          write(10, *) i, f(i)
+       end if
     end do
     close(10)
   end subroutine output
@@ -61,10 +63,10 @@ contains
 
     call rfr(N)
     do i = 0, N-1
-       if (ur(i) < 0.5d0) then
-          target_spin(i) = 0
-       else
+       if (ur(i) < 1d0/p) then
           target_spin(i) = 1
+       else
+          target_spin(i) = 0
        end if
     end do
   end subroutine init_spin
@@ -79,7 +81,7 @@ contains
        spin0(i) = 1 - spin0(i)
        call calc_E(E2)
        df = f(E) - f(E2)
-       if (ur(mmc*2+1) < exp(df)) then
+       if (df > 0.d0 .or. ur(mmc*2+1) < exp(df)) then
           E = E2
        else
           spin0(i) = 1 - spin0(i)
@@ -116,8 +118,10 @@ contains
 
   integer function is_flat()
     integer :: i, hist_n
-    double precision :: min_hist=-1.d0, min_f=-1.d0
+    double precision :: min_hist, min_f
 
+    min_hist = -1.d0
+    min_f = -1.d0
     hist_n = 0
     do i = 0, N
        if (o(i) == 1) then
@@ -126,13 +130,17 @@ contains
           if (min_f < 0.d0 .or. min_f > f(i)) min_f = f(i)
        end if
     end do
-    f(:) = f(:) - min_f
+    do i = 0, N
+       if (o(i) == 1) then
+          f(i) = f(i) - min_f
+       end if
+    end do
     if (min_hist > sum(hist) * XX / hist_n) then
        is_flat = 1
     else
        is_flat = 0
     end if
-    write(*, "(i3, f8.4, i9)") ki, min_hist / sum(hist) * hist_n, hist_n
+    write(*, "(i3, f8.4, f8.4)") ki, min_hist / sum(hist) * hist_n, hist_n*1.d0/(N+1)
   end function is_flat
 
 end program main
